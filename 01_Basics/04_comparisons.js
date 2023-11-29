04d3db7b3a83324ddc4af72818b63e02b01de012
ajcoder42 <84202466+ajcoder42@users.noreply.github.com>
2023-11-29 00:14:36 +0000

    comparsions of data done

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,33 @@
+// console.log(2 < 1); // false
+// console.log(2 == 1); // false
+// console.log(2 > 1); // true
+// console.log(2 <= 1); // false
+// console.log(2 >= 1); // true
+// console.log(2 != 1); // true
+// console.log(2 === 2); // true
+
+// +++++++++++++++++++++++++++++++ Comparsion between different Datatypes ++++++++++++++++++++++++++++++++++
+
+// console.log("1" == 1); // true
+// console.log("00001" == 1); // true
+// console.log("00001" > 1); // false
+
+// console.log(null == 0); // false
+// console.log(null < 0); // false
+// console.log(null > 0); // false
+// console.log(null === 0); // false
+
+// console.log(undefined == 0); // false
+// console.log(undefined < 0); // false
+// console.log(undefined > 0); // false
+// console.log(undefined === 0); // false
+
+// console.log(null == undefined); // true
+// console.log(undefined === null); // false
+
+// console.log(true == 1); // true
+// console.log(false == 0); // true
+// console.log(true === 1); // false
+// console.log(true > 1); // false
+// console.log(true > 0); // true
+// console.log(false > 0); // false
